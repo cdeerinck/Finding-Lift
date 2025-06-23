6b952a7687bba2620b17842d5338c608d107e014
Chuck Deerinck <cjd9@me.com>
2025-06-23 14:03:19 -0700

    Initial Commit

---
+++ Finding Lift/Finding_LiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  Finding_LiftApp.swift
+//  Finding Lift
+//
+//  Created by Chuck Deerinck on 6/23/25.
+//
+
+import SwiftUI
+
+@main
+struct Finding_LiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Finding LiftTests/Finding_LiftTests.swift
@@ -0,0 +1,17 @@
+//
+//  Finding_LiftTests.swift
+//  Finding LiftTests
+//
+//  Created by Chuck Deerinck on 6/23/25.
+//
+
+import Testing
+@testable import Finding_Lift
+
+struct Finding_LiftTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
